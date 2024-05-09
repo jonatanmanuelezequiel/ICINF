6b080b91e200f9fb28881f5371a59e5074290f8b
jonatanmanuelezequiel <jonatanmanuelezequiel.vasquez@alumnos.ulagos.cl>
2024-05-09 14:36:19 -0400

    Add files via upload

---
+++ sueldo.py
@@ -0,0 +1,6 @@
+valor_hora=int(input("ingrese valor por hora: "))
+horas_trabajadas_mes=int(input("ingrese horas trabajadas al mes: "))
+sueldo_dia=valor_hora*8
+sueldo_mensual=horas_trabajadas_mes*valor_hora
+print("el sueldo diario del trabajador es: ",sueldo_dia)
+print("el sueldo mensual del trabajador es: ",sueldo_mensual)
